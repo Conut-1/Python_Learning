87c0c625dc5cf0f90396121eb6243de54807c144
Conut-1 <1mim1@naver.com>
2024-03-19 13:34:38 +0900

    feat: 백준 문제 업로드

---
+++ 백준/문제/Gold/14728. 벼락치기.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def solve():
+    n, t = map(int, input().split())
+    dp = [0] * (t + 1)
+    for _ in range(n):
+        k, s = map(int, input().split())
+        for i in range(t, k - 1, -1):
+            dp[i] = max(dp[i - k] + s, dp[i])
+
+    print(max(dp))
+
+
+solve()
